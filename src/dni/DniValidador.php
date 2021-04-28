685f77cc6e0bc042571a24df43a5e159aba1b2ed
jca <jca@alzago.com>
2021-04-28 10:42:24 +0200

    Refactorizo: creo el método privado comprobarLongitudCorrecta

--- src/dni/DniValidador.php
+++ src/dni/DniValidador.php
@@ -10,10 +10,19 @@ class DniValidador {
     private const LONGITUD_VALIDA = 9;
 
     public function __construct(string $dni){
-        if (strlen($dni) !== self::LONGITUD_VALIDA){
-            throw new LengthException('longitud incorrecta');    
-        }
+        $this->comprobarLongitudCorrecta($dni);
+
         throw new DomainException('dni termina en número');
     }
 
+    /**
+     * @param string $dni dni suministrado
+     * @throws LengthException cuando el $dni no tiene el 
+     *                         tamaño self::LONGITUD_VALIDA
+     */
+    private function comprobarLongitudCorrecta(string $dni){
+        if (strlen($dni) !== self::LONGITUD_VALIDA){
+            throw new LengthException('longitud incorrecta');    
+        }
+    }
 }
